4e4f50ab2d82055eadf43c7647f90b553c0686c5
Alex <alexcarrascosalvador@gmail.com>
2022-01-22 08:41:41 +0100

    ui components created

--- apps/rick-and-morty/src/app/app.module.ts
+++ apps/rick-and-morty/src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('@workspace/rick-and-morty/characters').then((m) => m.MiGeneraliMainFeatureModule),
+  },
+  { path: '**', redirectTo: '' },
+];
+
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
